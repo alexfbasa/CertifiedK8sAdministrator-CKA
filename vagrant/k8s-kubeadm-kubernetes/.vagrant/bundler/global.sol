5e99f140f6332b9ce4f5a55bd4ae4e316e9d77dd
AlexBasa <alexsimple@gmail.com>
2024-01-05 21:10:57 +0200

    Adicionar pasta 'vagrant'

---
+++ vagrant/k8s-kubeadm-kubernetes/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]]],"checksum":"35ba93dcdb5863c7b1ff262efe0fa1bcf27c478b914757a36cc2c2ae640522dd","vagrant_version":"2.4.0"}
---
+++ vagrant/k8s-kubeadm-kubernetes/configs/join.sh
@@ -0,0 +1,1 @@
+kubeadm join 10.0.0.10:6443 --token y6lmez.rdgos365wq8can6x --discovery-token-ca-cert-hash sha256:ef016b87f961e8d29e4a6c17a3de2bca11a433555fae522f820575bfe6f499e2 
